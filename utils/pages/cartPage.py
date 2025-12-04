66e4203e36fa408ff797a832cec56aa0d9273f61
Bartosz_Automated_Dev <bartekfc41@gmail.com>
2025-12-04 19:51:54 +0100

    add_item test to finish

---
+++ utils/pages/cartPage.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, Locator, expect
+
+class CartPage:
+    def __init__(self, page: Page):
+        self.page = page
+
+    def validate_item(self, item: str) -> None:
+        locate_item: Locator = self.page.get_by_text(item)
+        expect(locate_item).to_be_visible()
